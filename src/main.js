ebada28aa475a971607d52e105cfa8bf7f2a3f8d
Tom <hty1251817231@gmail.com>
2021-02-14 21:41:33 +0800

    asd

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+import 'postcss-pxtorem'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
